179688f068d39dfeb9838144ccfd0be97e354e09
Imer Coaguila <imertpro@gmail.com>
2023-07-28 18:05:05 -0400

    languages added

--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -9,7 +9,7 @@ export class SharedService{
   constructor() { }
   private containerState = new BehaviorSubject<boolean>(false);
   private containerComponent = new BehaviorSubject<string>('');
-  // Observable para que los componentes puedan suscribirse y recibir actualizaciones
+
   containerState$ = this.containerState.asObservable();
   containerComponent$ = this.containerComponent.asObservable();
   // Método para actualizar el estado de la sección container
