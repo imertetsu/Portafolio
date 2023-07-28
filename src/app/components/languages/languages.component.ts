4a4e4fd26b681530f14aac00523bba5bdc361443
Imer Coaguila <imertpro@gmail.com>
2023-07-28 18:14:13 -0400

    languages optimized

--- src/app/components/languages/languages.component.ts
+++ src/app/components/languages/languages.component.ts
@@ -9,6 +9,19 @@ import { SharedService } from 'src/app/services/shared.service';
 export class LanguagesComponent {
   constructor(private sharedService: SharedService){}
 
+  images = [
+    '/assets/images/languages/js.png',
+    '/assets/images/languages/typescript.png',
+    '/assets/images/languages/nodejs.png',
+    '/assets/images/languages/expressIcon.png',
+    '/assets/images/languages/java.png',
+    '/assets/images/languages/spring.png',
+    '/assets/images/languages/file-type-angular.svg',
+    '/assets/images/languages/heroku_original_wordmark_logo_icon_146482.png',
+    '/assets/images/languages/docker.webp',
+    '/assets/images/languages/github.png'
+  ]
+
   onMouseEnter() {
     this.sharedService.updateContainerState(true, 'techs');
   }
