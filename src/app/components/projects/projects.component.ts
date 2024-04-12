53c5f6604023a9389b461a4823f236b69e001d5e
imertetsu <imertpro@gmail.com>
2024-04-12 00:25:42 -0400

    Parent App updated

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -23,7 +23,7 @@ export class ProjectsComponent {
         '/assets/projects/backendPizzeria/swaggerui3.PNG',
       ],
       source: 'https://github.com/imertetsu/pizzeria',
-      url: 'https://pizzeria.imertetsu.me/api/swagger-ui/index.html',
+      url: 'https://service.pizzeria.imertetsu.me/api/swagger-ui/index.html',
       projectDescription: {
         name: "Pizzeria API",
         description: "The Pizzeria API is an API designed to manage the entire ordering and management process of a pizzeria. From user management to creating and tracking pizza orders, the app offers a comprehensive solution to meet the needs of customers and staff alike.",
@@ -71,40 +71,36 @@ export class ProjectsComponent {
         '/assets/projects/sistemaImages/padresPage.PNG',
         '/assets/projects/sistemaImages/busesPge.PNG',
       ],
-      source: 'https://github.com/imertetsu/SchoolBusParent',
-      url: null,
+      source: null,
+      url: "https://service.busescolartracking.imertetsu.me",
       projectDescription: {
-        name: "Bus Escolar Tracker",
-        description: "La aplicación de Pizzería es una plataforma robusta diseñada para gestionar todo el proceso de pedidos y gestión de una pizzería. Desde la administración de usuarios hasta la creación y seguimiento de órdenes de pizzas, la aplicación ofrece una solución integral para satisfacer las necesidades de clientes y personal por igual.",
-        features: [
+        name: "School Bus Tracker",
+        description: "The School Bus Tracker is an application designed to connect two main applications: the parent app and the driver app. Its main purpose is to facilitate school route management, allowing drivers to transmit their real-time location through the driver app, and parents to view this location in the parent app.",
+        "features": [
           {
-            name: "Gestión de Usuarios",
-            description: "La aplicación permite la gestión de distintos tipos de usuarios, incluyendo administradores, clientes y chefs. Cada tipo de usuario tiene acceso a funcionalidades específicas de acuerdo a su rol en el sistema."
+            name: "Schools, Drivers, and Parents Management",
+            description: "The application allows registering and managing information about schools, drivers, and parents. This includes details such as names, contacts, and specific route and schedule assignments."
           },
           {
-            name: "Catálogo de Pizzas",
-            description: "La plataforma cuenta con un amplio catálogo de pizzas disponibles para que los clientes elijan y realicen sus pedidos."
+            name: "Real-Time Location Broadcasting",
+            description: "Drivers can broadcast their real-time location through the driver app. This information is constantly updated and available for viewing by parents in the parent app."
           },
           {
-            name: "Órdenes de Pizzas",
-            description: "Los clientes pueden realizar y rastrear sus órdenes de pizzas a través de la aplicación. Los chefs reciben notificaciones en tiempo real sobre los pedidos entrantes y pueden actualizar el estado de preparación de las pizzas."
+            name: "School Route Tracking",
+            description: "Parents can track school bus routes in real-time through the parent app. This allows them to know the current location of the bus and estimate arrival time at the designated stop."
+          },
+          // {
+          //   name: "Alerts and Notifications",
+          //   description: "The application can send alerts and notifications to parents about important events related to school routes, such as changes in departure times, late arrivals, or route cancellations."
+          // },
+          {
+            name: "Security and Privacy",
+            description: "Security measures are implemented to protect users' personal information, such as contact details and locations. Proper authentication and authorization ensure that only authorized users can access relevant information."
           },
           {
-            name: "Endpoints RESTful",
-            description: "La aplicación implementa endpoints RESTful para realizar operaciones CRUD (Create, Read, Update, Delete) en entidades clave como usuarios, pizzas y órdenes de pizzas. Estos endpoints admiten los métodos HTTP estándar, incluyendo GET, POST, PUT y DELETE."
+            name: "Intuitive and User-Friendly Interface",
+            description: "Both the parent app and the driver app feature intuitive and user-friendly interfaces. This facilitates navigation and use of the available functionalities for users."
           },
-          {
-            name: "Seguridad Basada en Roles",
-            description: "Se ha implementado un sistema de seguridad robusto que asigna roles a los usuarios, como administrador, cliente y chef. Cada rol tiene acceso restringido a ciertas funcionalidades y datos de la aplicación."
-          },
-          {
-            name: "Arquitectura Stateless",
-            description: "La aplicación sigue una arquitectura stateless, lo que significa que no almacena ningún estado de sesión en el servidor. En su lugar, utiliza tokens JWT (JSON Web Tokens) para autenticar y autorizar solicitudes de usuarios. Estos tokens se incluyen en los encabezados de las solicitudes HTTP para mantener la seguridad y la integridad de los datos."
-          },
-          {
-            name: "Endpoints Públicos",
-            description: "Aunque la mayoría de los endpoints requieren autenticación, la aplicación también cuenta con algunos endpoints públicos que pueden ser accedidos sin autenticación, como la consulta del catálogo de pizzas."
-          }
         ]
       }
     },
@@ -120,40 +116,32 @@ export class ProjectsComponent {
         '/assets/projects/sistemaImages/appPadre/configuracionUbi.PNG',
         '/assets/projects/sistemaImages/appPadre/registroHijo.PNG',
       ],
-      source: 'https://github.com/imertetsu/SchoolBusParent',
+      source: null,
       url: null,
       projectDescription: {
-        name: "Bus Escolar Tracker",
-        description: "La aplicación de Pizzería es una plataforma robusta diseñada para gestionar todo el proceso de pedidos y gestión de una pizzería. Desde la administración de usuarios hasta la creación y seguimiento de órdenes de pizzas, la aplicación ofrece una solución integral para satisfacer las necesidades de clientes y personal por igual.",
+        name: "Parent App",
+        description: "The Parent App is an integral part of the School Bus Tracker system, providing parents with essential features to ensure the safety and convenience of their children's transportation to and from school. Parents can log in using a registered phone number, enabling access to various functionalities designed to enhance their experience and peace of mind.",
         features: [
           {
-            name: "Gestión de Usuarios",
-            description: "La aplicación permite la gestión de distintos tipos de usuarios, incluyendo administradores, clientes y chefs. Cada tipo de usuario tiene acceso a funcionalidades específicas de acuerdo a su rol en el sistema."
+            name: "Phone Number Authentication",
+            description: "Parents can log in securely using their registered phone number, ensuring that only authorized users have access to the application."
           },
           {
-            name: "Catálogo de Pizzas",
-            description: "La plataforma cuenta con un amplio catálogo de pizzas disponibles para que los clientes elijan y realicen sus pedidos."
+            name: "Real-Time School Bus Location",
+            description: "Parents can view the real-time location of the school bus on a map within the app. This feature enables them to track the bus's progress and estimate arrival times accurately."
           },
           {
-            name: "Órdenes de Pizzas",
-            description: "Los clientes pueden realizar y rastrear sus órdenes de pizzas a través de la aplicación. Los chefs reciben notificaciones en tiempo real sobre los pedidos entrantes y pueden actualizar el estado de preparación de las pizzas."
+            name: "Pickup and Drop-off Location Management",
+            description: "Parents can set specific pickup and drop-off locations for their child within the app. This ensures that the bus driver knows exactly where to pick up and drop off the student, enhancing safety and efficiency."
           },
           {
-            name: "Endpoints RESTful",
-            description: "La aplicación implementa endpoints RESTful para realizar operaciones CRUD (Create, Read, Update, Delete) en entidades clave como usuarios, pizzas y órdenes de pizzas. Estos endpoints admiten los métodos HTTP estándar, incluyendo GET, POST, PUT y DELETE."
+            name: "Student Entry and Exit Records",
+            description: "The app maintains records of student entries and exits from the school bus, including timestamps and location details. This provides parents with valuable insights into their child's transportation habits and ensures accountability."
           },
           {
-            name: "Seguridad Basada en Roles",
-            description: "Se ha implementado un sistema de seguridad robusto que asigna roles a los usuarios, como administrador, cliente y chef. Cada rol tiene acceso restringido a ciertas funcionalidades y datos de la aplicación."
+            name: "User-Friendly Interface",
+            description: "The Parent App features a user-friendly interface designed for easy navigation and accessibility. Parents can quickly access relevant information and perform necessary actions without complications."
           },
-          {
-            name: "Arquitectura Stateless",
-            description: "La aplicación sigue una arquitectura stateless, lo que significa que no almacena ningún estado de sesión en el servidor. En su lugar, utiliza tokens JWT (JSON Web Tokens) para autenticar y autorizar solicitudes de usuarios. Estos tokens se incluyen en los encabezados de las solicitudes HTTP para mantener la seguridad y la integridad de los datos."
-          },
-          {
-            name: "Endpoints Públicos",
-            description: "Aunque la mayoría de los endpoints requieren autenticación, la aplicación también cuenta con algunos endpoints públicos que pueden ser accedidos sin autenticación, como la consulta del catálogo de pizzas."
-          }
         ]
       }
     },
@@ -168,10 +156,10 @@ export class ProjectsComponent {
         '/assets/projects/sistemaImages/appConductor/ubicacionConductor.jpeg',
         '/assets/projects/sistemaImages/appConductor/registroChild.jpeg',
       ],
-      source: 'https://github.com/imertetsu/SchoolBusParent',
+      source: null,
       url: null,
       projectDescription: {
-        name: "Bus Escolar Tracker",
+        name: "(SBT) Driver App",
         description: "La aplicación de Pizzería es una plataforma robusta diseñada para gestionar todo el proceso de pedidos y gestión de una pizzería. Desde la administración de usuarios hasta la creación y seguimiento de órdenes de pizzas, la aplicación ofrece una solución integral para satisfacer las necesidades de clientes y personal por igual.",
         features: [
           {
@@ -217,9 +205,9 @@ export class ProjectsComponent {
 
       ],
       source: null,
-      url: 'https://lenguas-ui.languages.imertetsu.me/',
+      url: 'https://ui.languages.imertetsu.me',
       projectDescription: {
-        name: "Aplicación de Pizzería",
+        name: "Lenguas Project",
         description: "The Pizzeria application is a platform designed to manage the entire ordering and management process of a pizzeria. From user management to creating and tracking pizza orders, the app offers a comprehensive solution to meet the needs of customers and staff alike.",
         features: [
           {
